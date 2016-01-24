c0f06d981ca1114e8ec3a229867c59a54c1a5dcc
Kyle Coberly <kyle.coberly@gmail.com>
2016-01-23 21:43:04 -0700

    Adding create book functionality

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require("serve-favicon");
 var logger = require("morgan");
 var cookieParser = require("cookie-parser");
 var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
 
 var routes = require("./routes/index");
 var books = require("./routes/books");
@@ -19,6 +20,13 @@ app.set("view engine", "hbs");
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator({
+    customValidators: {
+        isUrl: function(value){
+            return /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/.test(value);
+        }
+    }
+}));
 app.use(cookieParser());
 app.use(require("node-sass-middleware")({
     src: path.join(__dirname, "public"),
--- routes/books.js
+++ routes/books.js
@@ -2,11 +2,35 @@ var express = require("express");
 var router = express.Router();
 var databaseConnection = require("../database_connection");
 
-/* GET users listing. */
 router.get("/", function(request, response, next) {
     databaseConnection("book").select().then(function(books){
-        response.render("list_books", books);
+        response.render("list_books", {books: books});
     });
+});
+
+router.get("/new", function(request, response, next) {
+    response.render("add_book");
+});
+
+router.post("/", function(request, response, next) {
+    request.checkBody("title", "Title is empty or too long").notEmpty().isLength({max: 255});
+    request.checkBody("genre", "Genre is empty or too long").notEmpty().isLength({max: 255});
+    request.checkBody("description", "Description is missing or too long").notEmpty().isLength({max: 2000});
+    request.checkBody("cover_url", "Not a URL").isUrl();
+
+    var errors = request.validationErrors();
+    if (errors){
+        response.render("error", {errors: errors});
+    } else {
+        databaseConnection("book").insert({
+            title: request.body.title,
+            genre: request.body.genre,
+            description: request.body.description,
+            cover_url: request.body.cover_url
+        }).then(function(){
+            response.redirect("/books");
+        });
+    }
 });
 
 module.exports = router;
