e7e834e85643f49b07207708c39834c19078697d
Kyle Coberly <kyle.coberly@gmail.com>
2016-01-24 22:47:35 -0700

    Adding add author feature

--- routes/authors.js
+++ routes/authors.js
@@ -8,23 +8,23 @@ router.get("/new", function(request, response, next) {
 
 router.get("/:id", function(request, response, next) {
     databaseConnection("author")
-        .select()
-        .innerJoin("book_author", "author.id", "author_id")
-        .innerJoin("book", "book_id", "book.id")
+        .select("*", "author.id AS author_id")
+        .leftOuterJoin("book_author", "author.id", "author_id")
+        .leftOuterJoin("book", "book_id", "book.id")
         .where("author.id", request.params.id)
     .then(function(authors){
         var authors = mapBooksToAuthors(authors);
-        console.log(authors[0]);
         response.render("authors/get_author", {layouts: "authors_layout", author: authors[0]});
     });
 });
 
 router.get("/", function(request, response, next) {
     databaseConnection("author")
-        .select()
-        .innerJoin("book_author", "author.id", "author_id")
-        .innerJoin("book", "book_id", "book.id")
+        .select("*", "author.id AS author_id")
+        .leftOuterJoin("book_author", "author.id", "author_id")
+        .leftOuterJoin("book", "book_id", "book.id")
     .then(function(records){
+        console.log(records);
         var authors = mapBooksToAuthors(records);
         response.render("authors/list_authors", {layout: "authors_layout", authors: authors});
     });
@@ -32,9 +32,9 @@ router.get("/", function(request, response, next) {
 
 router.get("/delete/:id", function(request, response, next) {
     databaseConnection("author")
-        .select()
-        .innerJoin("book_author", "author.id", "author_id")
-        .innerJoin("book", "book_id", "book.id")
+        .select("*", "author.id AS author_id")
+        .leftOuterJoin("book_author", "author.id", "author_id")
+        .leftOuterJoin("book", "book_id", "book.id")
         .where("author.id", request.params.id)
     .then(function(authors){
         var authors = mapBooksToAuthors(authors);
@@ -44,9 +44,9 @@ router.get("/delete/:id", function(request, response, next) {
 
 router.get("/edit/:id", function(request, response, next) {
     databaseConnection("author")
-        .select()
-        .innerJoin("book_author", "author.id", "author_id")
-        .innerJoin("book", "book_id", "book.id")
+        .select("*", "author.id AS author_id")
+        .outerJoin("book_author", "author.id", "author_id")
+        .outerJoin("book", "book_id", "book.id")
         .where("author.id", request.params.id)
     .then(function(authors){
         response.render("authors/edit_author", {layout: "authors_layout", author: authors[0]});
@@ -54,20 +54,20 @@ router.get("/edit/:id", function(request, response, next) {
 });
 
 router.post("/", function(request, response, next) {
-    request.checkBody("title", "Title is empty or too long").notEmpty().isLength({max: 255});
-    request.checkBody("genre", "Genre is empty or too long").notEmpty().isLength({max: 255});
-    request.checkBody("description", "Description is missing or too long").notEmpty().isLength({max: 2000});
-    request.checkBody("cover_image_url", "Not a URL").isUrl(request.body.cover_url);
+    request.checkBody("first_name", "First name is empty or too long").notEmpty().isLength({max: 255});
+    request.checkBody("last_name", "Last name is empty or too long").notEmpty().isLength({max: 255});
+    request.checkBody("biography", "Biography is too long").isLength({max: 10000});
+    request.checkBody("portrait_url", "Not a URL").isUrl(request.body.portrait_url);
 
     var errors = request.validationErrors();
     if (errors){
         response.render("error", {errors: errors});
     } else {
         databaseConnection("author").insert({
-            title: request.body.title,
-            genre: request.body.genre,
-            description: request.body.description,
-            cover_url: request.body.cover_image_url
+            first_name: request.body.first_name,
+            last_name: request.body.last_name,
+            biography: request.body.biography,
+            portrait_url: request.body.portrait_url
         }).then(function(){
             response.redirect("/authors");
         });
